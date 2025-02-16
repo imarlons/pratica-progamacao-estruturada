e2feeedaf885a2e6e99988d0e9213a014edcf529
imarlons <marlon.silva@aluno.fmpsc.edu.br>
2025-02-16 18:46:31 -0300

    resolução do exercício 02

---
+++ Aula 01 - 10-02/EX02.py
@@ -0,0 +1,21 @@
+# EXERCÍCIO 02
+
+# linha para separar as informações e facilitar a visulização do programa no terminal
+linha = ('--------------------')
+# entrada
+primeiroValor = float(input('Infome o primeiro valor: ')) # o tipo da informação foi definido como float, caso algum número decimal seja informado
+segundoValor = float(input('Infome o segundo valor: '))
+
+# processamento
+soma = primeiroValor + segundoValor
+subtracao = primeiroValor  - segundoValor
+multiplicacao = primeiroValor * segundoValor
+divisao = primeiroValor / segundoValor
+print(linha)
+
+# saída
+print('Os valores informados foram {:.2f} e {:.2f}.'.format(primeiroValor, segundoValor))
+print(linha)
+print('O sistema processou os valores com sucesso!')
+print('Os resultados das operações são: \nSoma = {:.2f} \nSubtração = {:.2f} \nMultiplicação = {:.2f} \nDivisão = {:.2f}'.format(soma, subtracao, multiplicacao, divisao))
+print(linha)

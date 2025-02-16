785971a2167595475b80cbb6b11fa1923bd11b96
imarlons <marlon.silva@aluno.fmpsc.edu.br>
2025-02-16 18:58:40 -0300

    resolução do exercício 03

---
+++ Aula 01 - 10-02/EX03.py
@@ -0,0 +1,15 @@
+# EXERCÍCIO 03
+
+# linha para separar as informações e facilitar a visulização do programa no terminal
+linha = ('--------------------')
+# entrada
+numeroDeCavalos = int(input('Informe a quantidade de cavalos que você possui no seu haras: '))
+print(linha)
+
+# processamento
+calculo = numeroDeCavalos * 4
+numeroDeFerraduras = calculo
+
+# saída
+print('Você possui {} cavalos no seu haras. Logo, você precisará comprar {} ferraduras para realizar o cuidado de todos eles.'.format(numeroDeCavalos, numeroDeFerraduras))
+print(linha)

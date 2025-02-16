69578595e7e78ddec7f1cfc3a5fd557c5b2993ba
imarlons <marlon.silva@aluno.fmpsc.edu.br>
2025-02-16 19:41:55 -0300

    resolução do exercício 06

---
+++ Aula 01 - 10-02/EX06.py
@@ -0,0 +1,15 @@
+# EXERCÍCIO 06
+
+# linha para separar as informações e facilitar a visulização do programa no terminal
+linha = ('--------------------')
+# entrada
+pesoRefeicao = float(input('Informe o peso da refeição: '))
+print(linha)
+
+# processamento
+precoKg = 12 #O restaurante cobra R$12/kg
+valorCobranca =  pesoRefeicao * precoKg
+
+# saída
+print('Valor a pagar: R${:.2f}'.format(valorCobranca))
+print(linha)

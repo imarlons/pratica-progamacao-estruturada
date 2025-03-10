c447946df008d60badd6dcf6abf3c9109c882527
imarlons <marlon.silva@aluno.fmpsc.edu.br>
2025-03-10 09:32:51 -0300

    resolução do exercício 02

---
+++ Aula 02 - 10-03/EX02.py
@@ -0,0 +1,7 @@
+# EXERCÍCIO 02
+# 1. análise a linha de comando abaixo.
+# 2. altere um operador de linha, para tornar a resposta True
+# print(('a' == 'a' or 'b' == 'a') and ((1 > 1) and 2 <= 2))
+
+print(('a' == 'a' or 'b' == 'a') and ((1 >= 1) and 2 <= 2))
+

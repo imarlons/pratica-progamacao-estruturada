a4afc7311f1af6305313dbaef9826cb0e6d7aada
imarlons <marlon.silva@aluno.fmpsc.edu.br>
2025-03-23 22:24:48 -0300

    resolução dos exercícios da aula 03

---
+++ Aula 03 - 17-03/EX01.py
@@ -0,0 +1,7 @@
+# EXERCÍCIO 01
+
+# pesquise como tornar a primeira letra de uma string maiúscula.
+# frase: a tecnologia move o mundo
+
+frase = 'a tecnologia move o mundo'
+print(frase.capitalize())
---
+++ Aula 03 - 17-03/EX02.py
@@ -0,0 +1,13 @@
+# EXERCÍCIO 02
+
+# utilizando substring:
+# 1. imprima os caracteres nos índices pares.
+# 2. imprima os caracteres nos índices ímpares.
+
+nome = 'Ana Maria da Silva'
+
+par = nome[::2]
+print('Caracteres nos índices pares: {}'.format(par))
+impar = nome[1::2]
+print('Caracteres nos índices ímpares: {}'.format(impar))
+

dbb991b3b5776a0fd51ec80a37f240fad774436e
imarlons <marlon.silva@aluno.fmpsc.edu.br>
2025-04-20 16:51:14 -0300

    comentando o código para um exercício

--- Python Brasil/Lista de Exercícios/#2 Estrutura de Decisão/EX26.py
+++ Python Brasil/Lista de Exercícios/#2 Estrutura de Decisão/EX26.py
@@ -10,33 +10,37 @@
 # escreva um algoritmo que leia o número de litros vendidos, o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina), 
 # calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 2,50 o preço do litro do álcool é R$ 1,90.
 
+# valor dos combustíveis disponíveis para venda
 precoAlcool = 1.90
 precoGasolina = 2.50
 
-# entrada
+# entrada 
+# entrada de informações, aqui o usuário informará a quantiade de litros que ele deseja e o tipo de combustível
 litros = float(input('Quantos litros você deseja abastecer? '))
 tipoCombustivel = input('Digite o tipo de combustível! (A - Álcool, G - Gasolina): ').upper()
 
 # processamento
-if (tipoCombustivel == 'A'):
-    if (litros <= 20):
+# processamento de informações, aqui o sistema vai verificar o tipo de combustível e a quantiadade de combustível que será abastecido
+if (tipoCombustivel == 'A'): # se o combustível escolhido for A (Álcool), o programa entrará nesse bloco de execução.
+    if (litros <= 20): # se a quantidade de combustível abastecido for igual ou menor a 20 litros, o sistema aplicará uma quantidade X de desconto
         desconto = 0.03
-    else:
+    else: # se for maior, ele aplicará outro valor de desconto.
         desconto = 0.05 
-    valor = litros * precoAlcool * (1 - desconto)
+    valor = litros * precoAlcool * (1 - desconto)  # se o desconto for 3% → 1 - 0.03 = 0.97 | se for 5% → 1 - 0.05 = 0.95
 
-elif (tipoCombustivel == 'G'):
-    if (litros <= 20):
+elif (tipoCombustivel == 'G'): # se o combustível escolhido for G (Gasolina), o programa entrará nesse bloco de execução.
+    if (litros <= 20): # se a quantidade de combustível abastecido for igual ou menor a 20 litros, o sistema aplicará uma quantidade X de desconto
         desconto = 0.04 
-    else:
+    else: # se for maior, ele aplicará outro valor de desconto.
         desconto = 0.06
-    valor = litros * precoGasolina * (1 - desconto)
+    valor = litros * precoGasolina * (1 - desconto) # se o desconto for 4% → 1 - 0.04 = 0.96 | se for 6% → 1 - 0.06 = 0.94
 
-else:
+else: # se o tipo de combustível não for A ou G, o programa executará esse bloco de execução e finalizará o programa.
     print("Tipo de combustível inválido!")
     exit()
 
 # saída
+# se tudo tiver ocorrido bem e ter sido bem informado e executado, o programa vai retornar o valor que o cliente tem que pagar.
 print('O valor a ser pago é R${:.2f}'.format(valor))
 
 

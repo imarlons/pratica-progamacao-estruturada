5c84463e7cf42c817056e30c6a024d52c06571e5
imarlons <marlon.silva@aluno.fmpsc.edu.br>
2025-05-05 09:46:10 -0300

    a morte do MCB

--- Testes/For.py
+++ Testes/For.py
@@ -1,6 +1,7 @@
 # variável 'pai' para armazenar os nomes dos integrantes do grupo
-integrantesMCB = ['Marlon', 'Tiago', 'Vitor', 'Samuel', 'Bernardo', 'Felipe', 'Davi', 'Will']
+integrantesMCB = ['Marlon', 'Tiago']
 
 # versão simplificada da variável 'pai' para mostrar o nome de cada um de forma separada
 for integranteMCB in integrantesMCB:
     print('Olá, eu sou o', integranteMCB)
+
